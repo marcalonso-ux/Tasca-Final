a885672aed389f3d0eec54d431a663c83c053834
marcalonso@paucasesnovescifp.cat <marcalonsopol0708@gmail.com>
2026-04-29 11:50:24 +0200

    feat (JS/Index): Creado desplazamientos a traves de js del sidebar

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+function irCateg() {
+    window.location.href = "categories.html"; 
+}
+
+function IrCrear() {
+    window.location.href = "crear-tasca.html"
+}
+
+function IrIndex () {
+    window.location.href = "index.html"
+}
