96ac1b58cde5b1b8681508db2c418525c9054c58
marcalonso@paucasesnovescifp.cat <marcalonsopol0708@gmail.com>
2026-04-30 08:45:06 +0200

    feat(Index / CSS / JS): Creat part per afegit una nova categoria en la part de gestió de categories

--- JS/index.js
+++ JS/index.js
@@ -9,3 +9,12 @@ function IrCrear() {
 function IrIndex () {
     window.location.href = "index.html"
 }
+
+const picker = document.getElementById('color-picker');
+
+const actualizarColor = () => {
+  picker.style.backgroundColor = picker.value;
+};
+
+picker.addEventListener('input', actualizarColor);
+actualizarColor();
